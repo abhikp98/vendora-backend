994b06c630dc751b41b49da2d27e93b9719026cf
abhikp98 <kpabhinand16@gmail.com>
2026-03-08 17:41:30 +0530

    change base.py for whitenoice

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -13,6 +13,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic'
     'django.contrib.staticfiles',
 
     # Third party apps
