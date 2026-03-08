2b31c8f59ba728e0cd2ff0b17915f113e6ee9d10
abhikp98 <kpabhinand16@gmail.com>
2026-03-08 17:49:00 +0530

    changes for production errors

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -13,7 +13,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'whitenoise.runserver_nostatic'
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
 
     # Third party apps
